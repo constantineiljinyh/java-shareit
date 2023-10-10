254c169fa63a4962581584f4f11a7d68f32128ee
constantineiljinyh <122673134+constantineiljinyh@users.noreply.github.com>
2023-10-10 12:31:21 +0500

    sprint 14 try2

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -19,9 +20,10 @@ public class BookingDto {
     private Integer id;
 
     @NotNull(message = "Время начала бронирования должно быть заполнено")
-    @FutureOrPresent(message = "Время начала брони не может быть в прошлом")
+    @FutureOrPresent(message = "Время начала бронирования не может быть в прошлом")
     private LocalDateTime start;
 
+    @Future(message = "Время окончания бронирования не может быть в прошлом")
     @NotNull(message = "Время окончания бронирования должно быть заполнено")
     private LocalDateTime end;
 
